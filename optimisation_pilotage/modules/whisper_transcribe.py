9ebfc48859785334c49580e1f89ae278456072c4
Nagromc86 <morgan.75@hotmail.fr>
2025-09-02 14:25:26 +0200

    v2.2ok

--- optimisation_pilotage/modules/whisper_transcribe.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from faster_whisper import WhisperModel
-from .utils import MODELS_DIR
-
-def transcribe_file(wav_path: Path, model_size: str='small', device: str='cpu'):
-    model = WhisperModel(model_size, device=device, compute_type='int8', download_root=str(MODELS_DIR))
-    segments, info = model.transcribe(str(wav_path), vad_filter=True, vad_parameters=dict(min_silence_duration_ms=500))
-    text='\n'.join([seg.text.strip() for seg in segments])
-    return text, info
